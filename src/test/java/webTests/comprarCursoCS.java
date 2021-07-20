4cca7fc1420ecbea47968c45cf2a5a7dd030bcf3
i183280 <LEONARDO.GARCEZ@BRADESCO.COM.BR>
2021-07-19 22:57:28 -0300

    Incrementando a classe comprarCursoCS.java

--- src/test/java/webTests/comprarCursoCS.java
+++ src/test/java/webTests/comprarCursoCS.java
@@ -1,39 +1,77 @@
 package webTests;
 
-import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.E;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
 
 public class comprarCursoCS {
+    WebDriver driver;
+
+    @Before
+    public void iniciar() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/91/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+        System.out.println("0-Antes do Teste começar");
+    }
+
+    @After
+    public void finalizar() {
+        driver.quit();
+        System.out.println("Z-Depois do Teste finalizar");
+    }
+
     @Dado("^que acesso o site da Iterasys$")
     public void queAcessoOSiteDaIterasys() {
+        driver.get("https://www.iterasys.com.br");
+        System.out.println("1-Acessou o site");
     }
 
     @Quando("^pesquiso por \"([^\"]*)\"$")
-    public void pesquisoPor(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void pesquisoPor(String curso) {
+        driver.findElement(By.id("searchtext")).click();        //Opcional
+        driver.findElement(By.id("searchtext")).clear();       //Opcional
+        driver.findElement(By.id("searchtext")).sendKeys(Keys.chord(curso)); // escreve nome do curso letra p/ letra
+
+        System.out.println("2-Digitou o nome do curso " + curso);
     }
 
     @E("^clico na Lupa$")
     public void clicoNaLupa() {
+        driver.findElement(By.id("btn_form_search")).click();
+        System.out.println("3-Clicou na lupa");
     }
 
     @Entao("^vejo a lista de resultados para o curso \"([^\"]*)\"$")
-    public void vejoAListaDeResultadosParaOCurso(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void vejoAListaDeResultadosParaOCurso(String curso) {
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"" + curso + "\"");
+        System.out.println("4-Exibiu a lista de resultados para o curso " + curso);
+
     }
 
     @Quando("^clico em Matricule-se$")
     public void clicoEmMatriculeSe() {
+        driver.findElement(By.cssSelector("span.comprar")).click();
+        System.out.println("5-Clicou em matricul-se");
     }
 
     @Entao("^confirmo o nome do curso como \"([^\"]*)\" e o preco de \"([^\"]*)\"$")
-    public void confirmoONomeDoCursoComoEOPrecoDe(String arg0, String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void confirmoONomeDoCursoComoEOPrecoDe(String curso, String preco) {
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), curso);
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), preco);
+        System.out.println("6-Confirmou o nome " + curso + " e o preço do curso " + preco);
+
     }
 }
